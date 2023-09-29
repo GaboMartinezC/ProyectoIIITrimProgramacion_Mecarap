9513c24d65d988ec86b9fd97872cb5eebba873d5
gabo <@>
2023-09-29 07:49:15 -0600

    commit inicial

---
+++ ProyectoIIITrimProgramacion_Mecarap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoIIITrimProgramacion_Mecarap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
